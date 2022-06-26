a86035f24ce5a610f7afe2f05bdf5dae9fb9ef43
olaadesanya <olaadesanya19@gmail.com>
2022-06-26 11:53:46 -0700

    5- Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,7 @@
 /* using printf*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf ("with proper grammar, but the outcome is a piece of art,\n");
 
-return(0);
-
+return (0);
 }
