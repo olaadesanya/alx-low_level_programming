df6c3f4e6198f0053150aea73fd52ae422819a75
olaadesanya <olaadesanya19@gmail.com>
2022-06-26 23:23:19 -0700

    size is not grandure

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "sizes od various computer when run"
+ *
+ *Return: Always 0 (Success)
+ **/
 int main(void)
+
 {
-/* listing sizes*/
+
 printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
