bf71e311dee77268c7e287948908101dbbf6b946
olaadesanya <olaadesanya19@gmail.com>
2022-06-26 23:03:50 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
-/*
-*  using puts function
-*
-* main - Entry point
-*
-* Description: prints "Programming is like building a multilingual puzzle"
-*
-* Return: Always 0 (Success)
-*/
-
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-/* main funtion description*/
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
