ad4dbe58ce5e70b6093ab1c6f0b5df142a515e47
olaadesanya <olaadesanya19@gmail.com>
2022-07-05 13:50:46 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch = 'z' ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+
+
+
+
+}
