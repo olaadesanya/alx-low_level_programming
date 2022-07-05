451b91786830998448502512b9c11a2bf9f9242f
olaadesanya <olaadesanya19@gmail.com>
2022-07-05 13:15:38 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,13 +10,8 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('p');
+	_putchar('u');
 	_putchar('\n');
 
 	return (0);
