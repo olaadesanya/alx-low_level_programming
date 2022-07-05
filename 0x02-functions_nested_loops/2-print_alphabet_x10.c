d77db862388462b625e87441573444318d4f335d
olaadesanya <olaadesanya19@gmail.com>
2022-07-05 14:45:56 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * main - printing alphabets 10x
+ *
+ * retuen: void
+ */
+int print_alphabet(void)
+{
+	char alphabet_count = 0;
+	char ch;
+	while(alpha_count < 10)
+{
+	ch = 'a';
+
+}
+        while (ch <= 'z')
+{
+	_putchar(ch);
+	ch++
+}
+        alphabet_count++;
+	_putchar('\n');
+}
+
+
