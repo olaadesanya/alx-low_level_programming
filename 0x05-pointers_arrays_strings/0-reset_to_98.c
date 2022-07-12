7ba5c378cacd5efd0b5637bbfa1b874280195171
olaadesanya <olaadesanya19@gmail.com>
2022-07-12 10:26:05 -0700

    Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an integer parameter
- * @n: integer pointer
- * Return: 0 Success
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
 
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-	*n = 98;
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
